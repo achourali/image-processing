f67182d5b3650734af876e1fed5a62eb3fff167a
achourali <achourali1999@gmail.com>
2022-05-05 10:36:04 +0100

    +image statistics

--- Image.py
+++ Image.py
@@ -1,9 +1,11 @@
+import math
+
 class Image:
     matrix=None
     type=""
     width=0
     height=0
-    max=0
+    maxGray=0
     
     def __init__(self,pgmPath):
         file = open(pgmPath, 'rb')
@@ -13,17 +15,54 @@ class Image:
             line = file.readline()
         widthBinary, heightBinary = line.split()
         self.width, self.height = int(widthBinary), int(heightBinary)
-        self.max = int(file.readline())
+        self.maxGray = int(file.readline())
         self.matrix = []
         if(self.type == "P5"):
             for i in range(self.height):
                 row = list(file.read(self.width))
                 self.matrix.append(row)
         elif(self.type == "P2"):
-            for i in range(height):
+            for i in range(self.height):
                 line = file.readline()
                 row = line.split()
                 row = list(map(int, row))
                 self.matrix.append(row)
         
         file.close()
+        
+    def average(self):
+        sum = 0
+        for h in range(self.height):
+            for w in range(self.width):
+                sum += self.matrix[h][w]
+        return sum / (self.width*self.height)
+    
+    
+    def standard_deviation(self):
+        average = self.average()
+        sum = 0
+        for h in range(self.height):
+            for w in range(self.width):
+                sum += (self.matrix[h][w] - average) ** 2
+        return math.sqrt(sum / (self.width*self.height))
+    
+    
+    def histogram(self):
+        histogram = [0] * (self.maxGray + 1)
+        for h in range(self.height):
+            for w in range(self.width):
+                histogram[self.matrix[h][w]] += 1
+        return histogram
+    
+    
+    def cumulated_histogram(self):
+        histogram = self.histogram()
+        cumulated_histogram = [0] * (self.maxGray + 1)
+        cumulated_histogram[0] = histogram[0]
+        for i in range(1, self.maxGray + 1):
+            cumulated_histogram[i] = histogram[i] + cumulated_histogram[i - 1]
+        return cumulated_histogram
+
+
+
+        
--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from Image import Image
 path1='samples/github.pgm'
 path2="samples/feep.ascii.pgm"
 
-image1=Image(path1)
-print(image1.height)
+image=Image(path2)
+print(image.average())
+print(image.standard_deviation())
+print(image.histogram())
+print(image.cumulated_histogram())
 
 
